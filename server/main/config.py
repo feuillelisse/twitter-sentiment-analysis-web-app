8fb4b4be1265ef9d166f6033fb0d51abb1cde016
fachdf <fachridhia.f@gmail.com>
2022-05-13 09:34:17 +0700

    Init Setting Awal

--- server/main/config.py
+++ server/main/config.py
@@ -6,10 +6,10 @@ class BaseConfig:
     FLASK_APP="main/__init__.py"
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
     DEBUG = False
-    CONSUMER_KEY = '<YOUR TWITTER API CONSUMER KEY>'
-    CONSUMER_SECRET = '<YOUR TWITTER API CONSUMER SECRET>'
-    ACCESS_TOKEN = '<YOUR TWITTER API ACCESS TOKEN>'
-    ACCESS_TOKEN_SECRET = '<YOUR TWITTER ACCESS TOKEN SECRET>'
+    CONSUMER_KEY = 'EQWMnC47VbsTBvolrmVlVZJeB'
+    CONSUMER_SECRET = 'funxNWwRyCc3rTJWGss6SjJZgMhtVNIeemMyA59h1qqzK2fdGw'
+    ACCESS_TOKEN = '1514892570203418625-Vas7CHuYc2X8vJ8vJURNRhnxsHYpId'
+    ACCESS_TOKEN_SECRET = 'nMzjFmaiCJyt5S4zv9nQUKFnWSWgJzkgIZaUUyVsQ2FNO'
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration."""
