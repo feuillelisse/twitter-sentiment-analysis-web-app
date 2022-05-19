041a5e5ed221b776e01f09768ba1911ca64cc6f8
fachdf <fachridhia.f@gmail.com>
2022-05-19 23:22:06 +0700

    :wrench: Setting some env

--- client/src/App.js
+++ client/src/App.js
@@ -75,7 +75,7 @@ function App() {
 
     try {
       axios
-        .get("http://27.112.78.91:8000/analyzehashtag", {
+        .get("http://103.134.154.227:8000/analyzehashtag", {
           params: {
             text: term,
           },
@@ -109,7 +109,7 @@ function App() {
 
     try {
       axios
-        .get("http://27.112.78.91:8000/gettweets", {
+        .get("http://103.134.154.227:8000/gettweets", {
           params: {
             text: term,
           },
