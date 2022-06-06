6aaef8802eec9d583c57e2ee0bf50552f1710167
feuillelisse <nisashadrina@gmail.com>
2022-06-06 08:19:13 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -75,7 +75,7 @@ function App() {
 
     try {
       axios
-        .get("http://103.134.154.227:8000/analyzehashtag", {
+        .get("http://20.28.176.104:8000/analyzehashtag", {
           params: {
             text: term,
           },
@@ -109,7 +109,7 @@ function App() {
 
     try {
       axios
-        .get("http://103.134.154.227:8000/gettweets", {
+        .get("http://20.28.176.104:8000/gettweets", {
           params: {
             text: term,
           },
